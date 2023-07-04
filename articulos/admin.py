dd7717166369ac2cbbe77481723e0750ac70cff8
josehermosilla <jose.hermosillaa@usach.cl>
2023-07-04 19:59:59 -0400

    se crea modelo articulo y se agrega al panel de administrador

--- articulos/admin.py
+++ articulos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Article, ArticleAdmin)
